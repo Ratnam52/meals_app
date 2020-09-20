61b1140fa7b3efc8d93091ee77754a3eebd01734
Maniratnam <maniratnamrajalingam@gmail.com>
2020-09-20 18:05:10 +0530

    initial commit

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
+import './favorite_screen.dart';
+import './categories_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  TabsScreenState createState() => TabsScreenState();
+}
+
+class TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Category',
+    },
+    {
+      'page': FavoriteScreen(),
+      'title': 'Favorite',
+    }
+  ];
+
+  int selectedPageIndex = 0;
+
+  void selectPage(int index) {
+    setState(() {
+      selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(pages[selectedPageIndex]['title']),
+      ),
+      drawer: MainDrawer(),
+      body: pages[selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: selectedPageIndex,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
+        ],
+      ),
+    );
+  }
+}
